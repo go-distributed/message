8066b4e9e06fea723a3172f5288d2fb4710607fb
evan-gu <guyifan1121@gmail.com>
2013-12-25 00:21:19 -0500

    receiver and test

--- receiver.go
+++ receiver.go
@@ -1,7 +1,27 @@
 package message
 
+import (
+	"log"
+	"net"
+)
+
+const (
+	CHANSIZE = 10
+)
+
 type Receiver struct {
-	ch chan *Message
+	port string
+	ch   chan *Message
+	ln   net.Listener
+	stop chan bool
+}
+
+func NewReceiver(port string) *Receiver {
+	r := new(Receiver)
+	r.port = port
+	r.ch = make(chan *Message, CHANSIZE)
+	r.stop = make(chan bool, 1)
+	return r
 }
 
 func (r *Receiver) Recv() *Message {
@@ -17,6 +37,39 @@ func (r *Receiver) GoRecv() *Message {
 	}
 }
 
-func (r *Receiver) start() {
+func (r *Receiver) Start() {
+	ln, err := net.Listen("tcp", ":"+r.port) // only TCP now
+	r.ln = ln
+	if err != nil {
+		log.Fatal("Listen() error:", err)
+	}
 
+Loop:
+	for {
+		select {
+		case <-r.stop:
+			break Loop
+		default:
+			conn, err := ln.Accept()
+			if err != nil {
+				log.Println("Accept() error:", err)
+				continue
+			}
+			go func() {
+				msg := NewEmptyMessage()
+				d := NewMsgDecoder(conn)
+				err := d.Decode(msg)
+				if err != nil {
+					log.Println("Decode() error:", err)
+					return
+				}
+				r.ch <- msg
+			}()
+		}
+	}
+}
+
+func (r *Receiver) Stop() error {
+	r.stop <- true
+	return r.ln.Close()
 }
---
+++ receiver_test.go
@@ -0,0 +1,128 @@
+package message
+
+import (
+	"bytes"
+	"net"
+	"reflect"
+	"testing"
+	"time"
+
+	"code.google.com/p/gogoprotobuf/proto"
+	"github.com/go-epaxos/message/example"
+)
+
+func TestBlockRecv(t *testing.T) {
+	buf, inPb, msg := inittest(t)
+	tmpChan := make(chan *Message)
+	r := NewReceiver("8000")
+	go r.Start()
+	time.Sleep(2 * time.Second)
+
+	// should not receive anything
+	go func() {
+		tmpChan <- r.Recv()
+	}()
+
+Loop:
+	for {
+		select {
+		case <-time.After(3 * time.Second):
+			break Loop
+		case <-tmpChan:
+			t.Fatal("Recv should block!")
+		}
+	}
+
+	// start sending
+	go send("8000", buf, t)
+	outMsg := <-tmpChan
+	compareMsg(msg, outMsg, inPb, t)
+}
+
+func TestNonBlockRecv(t *testing.T) {
+	buf, inPb, msg := inittest(t)
+
+	// start receiver
+	r := NewReceiver("8001")
+	go r.Start()
+	time.Sleep(2 * time.Second) // wait for server to start
+
+	// should not receive anything
+	outMsg := r.GoRecv()
+	if outMsg != nil {
+		t.Fatal("GoRecv() return not nil")
+	}
+
+	// start sending
+	go send("8001", buf, t)
+	time.Sleep(2 * time.Second) // wait for socket to be available
+
+	outMsg = r.GoRecv()
+	if outMsg == nil {
+		t.Fatal("nil")
+	}
+	compareMsg(msg, outMsg, inPb, t)
+}
+
+// Test whether receiver can stop and listen again
+func TestStopRestart(t *testing.T) {
+	r := NewReceiver("8002")
+	go r.Start()
+	time.Sleep(2 * time.Second)
+
+	r.Stop()
+	go r.Start()
+}
+
+func inittest(t *testing.T) (*bytes.Buffer, *example.A, *Message) {
+	buf := new(bytes.Buffer)
+	inPb := &example.A{
+		Description: "hello world!",
+		Number:      1,
+	}
+	// UUID is 16 byte long
+	for i := 0; i < 16; i++ {
+		inPb.Id = append(inPb.Id, byte(i))
+	}
+
+	bytes, err := proto.Marshal(inPb)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msg := NewMessage(0, bytes)
+	e := NewMsgEncoder(buf)
+	e.Encode(msg)
+
+	return buf, inPb, msg
+}
+
+func send(port string, buf *bytes.Buffer, t *testing.T) {
+	conn, err := net.Dial("tcp", ":"+port)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	n, err := buf.WriteTo(conn)
+	if err != nil {
+		t.Log(n)
+		t.Fatal(err)
+	}
+}
+
+func compareMsg(msg, outMsg *Message, a interface{}, t *testing.T) {
+	if !reflect.DeepEqual(msg, outMsg) {
+		t.Fatal("Messages are not equal!")
+	}
+
+	switch reflect.TypeOf(a).String() {
+	case "*example.A":
+		outPb := new(example.A)
+		proto.Unmarshal(outMsg.bytes, outPb)
+		if !reflect.DeepEqual(outPb, outPb) {
+			t.Fatal("Protos are not equal!")
+		}
+	default:
+		t.Fatal("Unknow type")
+	}
+}
