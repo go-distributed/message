6857d1b851ee6d61b163798f940f5fdb384fdefb
Xiang Li <xiang.li@coreos.com>
2013-12-26 08:03:19 +0800

    feat(sender.go) finish a draft sender with simple test

--- sender.go
+++ sender.go
@@ -1,10 +1,56 @@
 package message
 
+import (
+	"net"
+)
+
 type Sender struct {
+	remoteAddr *net.TCPAddr
+	conn       *net.TCPConn
+	encoder    *MsgEncoder
+	decoder    *MsgDecoder
 }
 
-func (s *Sender) Send() *Message {
-	return NewEmptyMessage()
+func NewSender(remoteAddr string) (*Sender, error) {
+	raddr, err := net.ResolveTCPAddr("tcp", remoteAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.DialTCP("tcp", nil, raddr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Sender{
+		remoteAddr: raddr,
+		conn:       conn,
+		encoder:    NewMsgEncoder(conn),
+		decoder:    NewMsgDecoder(conn),
+	}, nil
+}
+
+func (s *Sender) Send(msg *Message) (*Message, error) {
+	err := s.encoder.Encode(msg)
+	// TODO: handle recoverable error...
+	if err != nil {
+		s.conn.Close()
+		s.conn = nil
+		return nil, err
+	}
+
+	if !msg.RequireReply() {
+		return nil, nil
+	}
+
+	reply := NewEmptyMessage()
+	err = s.decoder.Decode(reply)
+	if err != nil {
+		s.conn.Close()
+		s.conn = nil
+		return nil, err
+	}
+	return reply, nil
 }
 
 func (s *Sender) GoSend() {
