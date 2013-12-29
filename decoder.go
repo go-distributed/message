106d31bbbe2666d262175f90bb01eb98b9a53e58
Xiang Li <xiang.li@coreos.com>
2013-12-29 22:09:18 +0800

    integrate init

--- decoder.go
+++ decoder.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"encoding/binary"
 	"io"
+	"reflect"
+
+	"code.google.com/p/gogoprotobuf/proto"
 )
 
 type MsgDecoder struct {
@@ -14,6 +17,36 @@ func NewMsgDecoder(r io.Reader) *MsgDecoder {
 	return &MsgDecoder{
 		br: bufio.NewReader(r),
 	}
+}
+
+func (md *MsgDecoder) DecodePb(m *PbMessage) error {
+	msgType, err := md.br.ReadByte()
+
+	if err != nil {
+		return err
+	}
+
+	m.msgType = uint8(msgType)
+
+	t, ok := registry[m.msgType]
+
+	if !ok {
+		panic("unknown type")
+	}
+
+	v := reflect.New(t)
+	m.pb = v.Interface().(proto.Message)
+
+	var size uint32
+	err = binary.Read(md.br, binary.LittleEndian, &size)
+	if err != nil {
+		return err
+	}
+
+	bytes := make([]byte, size)
+	_, err = io.ReadFull(md.br, bytes)
+
+	return proto.Unmarshal(bytes, m.pb)
 }
 
 func (md *MsgDecoder) Decode(m *Message) error {
